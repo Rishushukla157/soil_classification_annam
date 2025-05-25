cd0a5d91b58cc7ec967e1b3f4929f5ac2984a6ca
Rishushukla157 <154089916+Rishushukla157@users.noreply.github.com>
2025-05-25 23:17:44 +0530

    Create preprocessing.py

---
+++ challenge 1/src/preprocessing.py
@@ -0,0 +1,14 @@
+"""
+
+Author: Annam.ai IIT Ropar
+Team Name: OverTakers
+Team Members: Member-1 rishu Shukla , member-2 Kaushik pal
+Leaderboard Rank: 55
+
+"""
+
+# Here you add all the preprocessing related details for the task completed from Kaggle.
+
+def preprocessing():
+    print("This is the file for preprocessing")
+  return 0
