816a0a68c74bea343030651a20b30372ae93d9d3
Rishushukla157 <154089916+Rishushukla157@users.noreply.github.com>
2025-05-26 00:03:29 +0530

    Update download.sh

--- challenge 2/data /download.sh
+++ challenge 2/data /download.sh
@@ -1,5 +1,5 @@
 Dataset or competition or notebook slug
 
 dataset: 'annam-ai/soilclassification/soil classification 2'
-competition: 'annam-ai/soilclassification'
+competition: 'annam-ai/soilclassification-Part2'
 notebook: 'AIMLD28_KAUSHIK PAL/notebookf0a784df7e'
