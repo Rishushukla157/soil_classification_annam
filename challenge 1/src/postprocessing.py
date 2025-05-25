961594b2657494522bce05d7f052d83c19de7a3a
Rishushukla157 <154089916+Rishushukla157@users.noreply.github.com>
2025-05-25 23:17:09 +0530

    Create postprocessing.py

---
+++ challenge 1/src/postprocessing.py
@@ -0,0 +1,14 @@
+"""
+
+Author: Annam.ai IIT Ropar
+Team Name: OverTakers
+Team Members: Member-1 rishu Shukla, member-2 Kaushik pal
+Leaderboard Rank: 55
+
+"""
+
+# Here you add all the post-processing related details for the task completed from Kaggle.
+
+def postprocessing():
+    print("This is the file for postprocessing")
+  return 0
