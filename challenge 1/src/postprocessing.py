1f756284e61b6e993eece06dac9a36692fbca8dc
Rishushukla157 <154089916+Rishushukla157@users.noreply.github.com>
2025-05-25 23:36:08 +0530

    Update postprocessing.py

--- challenge 1/src/postprocessing.py
+++ challenge 1/src/postprocessing.py
@@ -6,8 +6,20 @@ Team Members: Member-1 rishu Shukla, member-2 Kaushik pal
 Leaderboard Rank: 55
 
 """
+final_preds = []
+for img_id in image_ids:
+    # Stack fold predictions and average
+    fold_probs = np.stack(model_preds[img_id], axis=0)
+    avg_probs = np.mean(fold_probs, axis=0)
+    pred_label = label_encoder.classes_[np.argmax(avg_probs)]
+    final_preds.append(pred_label)
+submission = pd.DataFrame({
+    "image_id": image_ids,
+    "soil_type": final_preds
+})
 
-# Here you add all the post-processing related details for the task completed from Kaggle.
+submission.to_csv("submission.csv", index=False)
+print("Submission file created: submission.csv")
 
 def postprocessing():
     print("This is the file for postprocessing")
