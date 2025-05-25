0734e20f5a69db689c7a253af9c92a8d708e79ab
Rishushukla157 <154089916+Rishushukla157@users.noreply.github.com>
2025-05-25 23:36:57 +0530

    Update preprocessing.py

--- challenge 1/src/preprocessing.py
+++ challenge 1/src/preprocessing.py
@@ -5,9 +5,31 @@ Team Name: OverTakers
 Team Members: Member-1 rishu Shukla , member-2 Kaushik pal
 Leaderboard Rank: 55
 
-"""
+"""import os
+import numpy as np
+import pandas as pd
+from PIL import Image
+from tqdm import tqdm
 
-# Here you add all the preprocessing related details for the task completed from Kaggle.
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms
+from torchvision.models import efficientnet_b3, EfficientNet_B3_Weights
+
+from sklearn.model_selection import StratifiedKFold
+from sklearn.preprocessing import LabelEncoder
+import copy
+
+# Paths
+BASE_DIR = "/kaggle/input/soil-classification/soil_classification-2025"
+TRAIN_DIR = os.path.join(BASE_DIR, "train")
+TEST_DIR = os.path.join(BASE_DIR, "test")
+LABELS_CSV = os.path.join(BASE_DIR, "train_labels.csv")
+TEST_IDS_CSV = os.path.join(BASE_DIR, "test_ids.csv")
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+.
 
 def preprocessing():
     print("This is the file for preprocessing")
