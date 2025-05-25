3eb82c5579d447ceb8959474b68b83ef9f4c3731
Rishushukla157 <154089916+Rishushukla157@users.noreply.github.com>
2025-05-25 23:28:09 +0530

    Create download.sh

---
+++ challenge 2/data /download.sh
@@ -0,0 +1,5 @@
+Dataset or competition or notebook slug
+
+dataset: 'annam-ai/soilclassification/soil classification 2'
+competition: 'annam-ai/soilclassification'
+notebook: 'AIMLD28_KAUSHIK PAL/notebookf0a784df7e'
